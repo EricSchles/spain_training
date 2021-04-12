96c8033139dffa9816e5df7a09aa2358d773d61a
CadahiaDelgado <Pedro.CadahiaDelgado@pepsico.com>
2021-04-12 16:42:34 +0200

    We added a infix decorator for adder function

--- first_doc.py
+++ first_doc.py
@@ -1,3 +1,16 @@
+from functools import partial
+
+class Infix(object):
+    def __init__(self, func):
+        self.func = func
+    def __or__(self, other):
+        return self.func(other)
+    def __ror__(self, other):
+        return Infix(partial(self.func, other))
+    def __call__(self, v1, v2):
+        return self.func(v1, v2)
+
+@Infix        
 def adder(a: int, b: int) -> int:
     """
     # Description:
