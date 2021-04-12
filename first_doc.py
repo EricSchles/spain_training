e3bbacd8a4a6c74515460d1daf51cddf2de61d13
CadahiaDelgado <Pedro.CadahiaDelgado@pepsico.com>
2021-04-12 16:38:41 +0200

    We added documentation comment to the function adder

--- first_doc.py
+++ first_doc.py
@@ -1,2 +1,16 @@
 def adder(a: int, b: int) -> int:
+    """
+    # Description:
+    -------------
+    This function helps to sum 2 int numbers
+
+    # Inputs:
+    -------------
+    * a : float - The first parameter to sum
+    * b : float - The second parameter to sum.
+    
+    # Output
+    -------------
+    The sum of both numbers a and b. which is an int.
+    """
     return a+b
